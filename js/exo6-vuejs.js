8115be85422f49d2002f96ea3ced8feed7cae95f
CathyThuliez <popschool@debian>
2017-08-03 12:17:46 +0200

    Add app6 vue.js

---
+++ js/exo6-vuejs.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+   console.log("DOM fully loaded and parsed");
+   var app6 = new Vue({
+     el: '#app-6',
+     data: {
+       message: 'Hello ! Do you like Vue ? '
+     }
+   })
+});
