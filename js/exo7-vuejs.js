792ce2560fb784e31a0bf6e689221620b5d71fbc
CathyThuliez <cathy.thuliez@crea-tech.fr>
2017-08-08 13:40:09 +0200

    Add app7

--- js/exo7-vuejs.js
+++ js/exo7-vuejs.js
@@ -2,6 +2,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    console.log("DOM fully loaded and parsed");
    Vue.component('todo-item', {
-     template: '<li>Ceci est une liste</li>'
+     props: ['todo'],
+     template: '<li>{{ todo.text }}</li>'
+   })
+   var app7 = new Vue({
+     el: '#app-7',
+     data: {
+       groceryList: [
+         { id: 0, text: 'Légumes' },
+         { id: 1, text: 'Fromage' },
+         { id: 2, text: 'Tout ce que les humains sont supposés manger' }
+       ]
+     }
    })
 });
